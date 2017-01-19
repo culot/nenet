191043db05983d55ae4c0fad7add11cb437b6f04
culot <culot@FreeBSD.org>
2017-01-19 19:41:17 +0100

    Forgot some basic pragma stuff in header files...

--- actvfunc.h
+++ actvfunc.h
@@ -24,6 +24,8 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#pragma once
+
 #include <string>
 
 namespace nenet {
--- softsign.h
+++ softsign.h
@@ -24,6 +24,8 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#pragma once
+
 #include "actvfunc.h"
 
 namespace nenet {
